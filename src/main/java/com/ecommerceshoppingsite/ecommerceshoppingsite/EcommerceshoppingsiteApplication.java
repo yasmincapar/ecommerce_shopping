96493ca1d533c0d78df2e39fa9b38765828996a6
yasmincapar <y.capar@yahoo.com>
2023-05-17 07:49:01 +0100

    adding packages

---
+++ src/main/java/com/ecommerceshoppingsite/ecommerceshoppingsite/EcommerceshoppingsiteApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerceshoppingsite.ecommerceshoppingsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceshoppingsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceshoppingsiteApplication.class, args);
+	}
+
+}
